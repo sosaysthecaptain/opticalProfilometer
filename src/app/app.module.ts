4b316d1b4f2d4263906895f73e9c3b7b3c5d8159
Marc Auger <sosaysthecaptain@gmail.com>
2018-03-21 20:39:00 -0500

    firebase set up again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,22 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAfQYpSeeC27ZJuAHIRtBdbCN63Zvfr9SE",
+  authDomain: "profilometer-cc55d.firebaseapp.com",
+  databaseURL: "https://profilometer-cc55d.firebaseio.com",
+  projectId: "profilometer-cc55d",
+  storageBucket: "profilometer-cc55d.appspot.com",
+  messagingSenderId: "538361946410"
+};
 
 
 @NgModule({
@@ -19,7 +32,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     BrowserModule,
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/Command.ts
@@ -0,0 +1,6 @@
+export interface Command {
+    id?:string;
+    body:string,
+    status:string,
+    timeStamp:number;
+  }
